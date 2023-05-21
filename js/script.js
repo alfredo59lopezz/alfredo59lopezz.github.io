da92c019ffeabce8cf682fe10c8581bc9fdfb7b3
alfredo59lopezz <114368889+alfredo59lopezz@users.noreply.github.com>
2023-05-21 06:23:37 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -27,11 +27,13 @@ function changePage(page) {
     }
 }
 
-/* function back space*/
-document.addEventListener("keydown", function (event) {
-    if (event.keyCode === 8) { // Código de tecla para Backspace
-        event.preventDefault(); // Evitar el comportamiento predeterminado de la tecla Backspace (ir hacia atrás en el historial)
-        history.back(); // Retroceder en el historial del navegador
+/* function back space */
+document.addEventListener('keydown', function (event) {
+    if (event.key === "Backspace") {
+        if (event.target.tagName !== "INPUT" && event.target.tagName !== "TEXTAREA") {
+            event.preventDefault();
+            window.history.back();
+        }
     }
 });
 
@@ -39,89 +41,13 @@ document.addEventListener("keydown", function (event) {
 
 
 
-function validatedForm() {
-    var form = document.getElementById("form-contact");
-
-    var labelErrorName = document.getElementById("name-label-error");
-    var labelErrorEmail = document.getElementById("email-label-error");
-    var labelErrorMensaje = document.getElementById("mensaje-label-error");
-
-    var valid = true;
-
-
-    /*  me funciona para validar que el campo no este vacio
-        var regex = /^[a-z A-Z]+$/;
-        if (!regex.test(form.nombre.value)) {
-            labelErrorName.style.display = 'block';
-            valid = false;
-        } else {
-            labelErrorName.style.display = 'none';
-        }*/
-
-
-    if (form.nombre.value.lenght == 0) {
-        labelErrorName.style.display = 'block'; //lanzar el error
-        valid = false;
-    } else {
-        labelErrorName.style.display = 'none';
-    }
 
 
 
-    var validEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
-    if (!validEmail.test(form.email.value)) {
-        labelErrorEmail.style.display = 'block';
-        valid = false;
-    } else {
-        labelErrorEmail.style.display = 'none'
-    }
-    if (form.mensaje.value.lenght == 0) {
-        labelErrorMensaje.style.display = 'block'
-        valid = false;
-    } else {
-        labelErrorMensaje.style.display = 'none';
-    }
-    valid ? alert("Formulario enviado con exito") : false;
-
-}
-
-/*
-function myFunction() {
-    // Get the value of the input field with id="numb"
-    let x = document.getElementById("numb").value;
-    // If x is Not a Number or less than one or greater than 10
-    let text;
-    if (isNaN(x) || x < 1 || x > 10) {
-        text = "Input not valid";
-    } else {
-        text = "Input OK";
-    }
-    document.getElementById("demo").innerHTML = text;
-}
-<div>
-    <h2>JavaScript Validation</h2>
-
-    <p>Please input a number between 1 and 10:</p>
-
-    <input id="numb">
-
-        <button type="button" onclick="myFunction()">Submit</button>
-
-        <p id="demo"></p>
-</div>*/
 
 
 
-/* funciona mosttrar mas imagenes*/
-/*function showMoreImages() {
-    const hiddenImages = document.querySelectorAll('.gallery-img.hide');
 
-    for (let i = 0; i < hiddenImages.length; i++) {
-        hiddenImages[i].classList.remove('hide');
-    }
-
-    document.querySelector('.show-more').style.display = 'none';
-}*/
 
 
 
